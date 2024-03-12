cf2382ef5e75ce4863f8ac956efb158a4cc319a7
TheBababoi <georgekamidis@gmail.com>
2024-03-12 15:12:49 +0200

    Injected Custom application.properties

--- src/main/java/com/springboot/demo/myapp/rest/RestController.java
+++ src/main/java/com/springboot/demo/myapp/rest/RestController.java
@@ -1,11 +1,24 @@
 package com.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    // exposes "/" that returns "Hello World"
+    // inject properties
+    @Value("${workout.day}")
+    private String workoutDay;
+    @Value("${muscle.group}")
+    private String muscleGroup;
+
+    // expose new endpoint for workoutDay
+    @GetMapping("/workoutDay")
+    public String workoutDay(){
+        return "Time for a " + workoutDay + " Workout! The muscle groups that are going to get trained today are: " + muscleGroup;
+    }
+
+    // expose "/" that returns "Hello World"
 
     @GetMapping
     public String sayHello(){
